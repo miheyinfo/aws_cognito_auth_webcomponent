c682a5bb0ae1adb96a20f45f93fa414d50c29cf4
miheyinfo <miheyinfo@gmail.com>
2022-02-18 23:52:40 +0100

    simple Internationalization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import { I18n } from 'aws-amplify';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'aws-cognito-auth-webcomponent';
+
+
+  ngOnInit(): void {
+    const dict = {
+      'fr': {
+        'Sign In': "Se connecter",
+        'Sign Up': "S'inscrire"
+      },
+      'es': {
+        'Sign In': "Registrarse",
+        'Sign Up': "Regístrate"
+      }
+    };
+
+    I18n.putVocabularies(dict)
+
+    I18n.setLanguage('fr');
+  }
+
+
 }
